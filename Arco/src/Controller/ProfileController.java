afcdf7fd6d7ec7a59b3ea330e63f4f6e45843fe8
DMCDavi <davi.machado.costa@hotmail.com>
2019-11-02 00:40:14 -0300

    telas de psicologo e perfil

--- Arco/src/Controller/ProfileController.java
+++ Arco/src/Controller/ProfileController.java
@@ -1,36 +1,105 @@
+/*
+Pendencias:
+    - Receber e exibir os dados de cadastro do usuario;
+    - Alterar os dados no banco quando o usuario salvar;
+*/
+
 package Controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
 
 public class ProfileController implements Initializable {
+
+    @FXML
+    ChoiceBox chSex, chGender;
+
+    @FXML
+    TextField name, mail, password, confPassword;
 
     @FXML
     protected void homeOnAction() {
         View.Main.changeScreen("Dashboard");
     }
+
     @FXML
     protected void lawOnAction() {
         View.Main.changeScreen("Law");
     }
+
     @FXML
     protected void calendarOnAction() {
         View.Main.changeScreen("Calendar");
     }
+
     @FXML
     protected void psychoOnAction() {
         View.Main.changeScreen("Psycho");
     }
+
     @FXML
     protected void exitOnAction() {
         View.Main.changeScreen("SignIn");
     }
+    
+    private void sexBox() {
+        List<String> sexList = new ArrayList<String>();
+        Iterator<String> it = sexList.iterator();
+        sexList.add("Masculino");
+        sexList.add("Feminino");
+        sexList.add("Intersexo");
+        sexList.add("Outro");
+        sexList.add("Prefiro não informar");
+        sexList.stream().forEach((sex) -> {
+            chSex.getItems().add(sex);
+        });
+    }
+    
+    private void genderBox() {
+        List<String> genderList = new ArrayList<String>();
+        Iterator<String> it = genderList.iterator();
+        genderList.add("Heterossexual");
+        genderList.add("Lésbica");
+        genderList.add("Gay");
+        genderList.add("Bissexual");
+        genderList.add("Transgênero");
+        genderList.add("Two-Spirit");
+        genderList.add("Queer");
+        genderList.add("Assexuado");
+        genderList.add("Panssexual");
+        genderList.add("Pangênero");
+        genderList.add("Bigênero");
+        genderList.add("Variante");
+        genderList.add("Sem gênero");
+        genderList.add("Não sei");
+        genderList.add("Outro");
+        genderList.add("Prefiro não informar");
+        genderList.stream().forEach((gender) -> {
+            chGender.getItems().add(gender);
+        });
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        sexBox();
+        genderBox();
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(4.0f);
+        ds.setColor(Color.color(0.6f, 0.6f, 0.6f));
+        name.setEffect(ds);
+        mail.setEffect(ds);
+        chSex.setEffect(ds);
+        chGender.setEffect(ds);
+        password.setEffect(ds);
     }
 
 }
--- Arco/src/Controller/PsychoController.java
+++ Arco/src/Controller/PsychoController.java
@@ -1,28 +1,55 @@
+/*
+Pendencias:
+    - Conectar as ChoiceBoxes ao banco para pegar as informacoes;
+    - Adicionar data e horario no calendario;
+    - Nao deixar o usuario marcar numa data anterior;
+    - Nao deixar o usuario marcar no mesmo dia e horario de outra consulta ja marcada;
+    - Nao deixar o usuario inserir horarios invalidos;
+*/
+
 package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
 
 public class PsychoController implements Initializable {
+
+    @FXML
+    ChoiceBox institution, psycho;
+    
+    @FXML
+    TextField hour, minute;
+    
+    @FXML
+    DatePicker date;
 
     @FXML
     protected void homeOnAction() {
         View.Main.changeScreen("Dashboard");
     }
+
     @FXML
     protected void lawOnAction() {
         View.Main.changeScreen("Law");
     }
+
     @FXML
     protected void calendarOnAction() {
         View.Main.changeScreen("Calendar");
     }
+
     @FXML
     protected void profileOnAction() {
         View.Main.changeScreen("Profile");
     }
+
     @FXML
     protected void exitOnAction() {
         View.Main.changeScreen("SignIn");
@@ -30,6 +57,14 @@ public class PsychoController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(4.0f);
+        ds.setColor(Color.color(0.6f, 0.6f, 0.6f));
+        institution.setEffect(ds);
+        psycho.setEffect(ds);
+        hour.setEffect(ds);
+        minute.setEffect(ds);
+        date.setEffect(ds);
 
     }
 
